535f985b4002a7e48a5728c1abbe8093ba063a36
mmmm <599397252@qq.com>
2019-12-16 15:55:37 +0800

    mmmm

---
+++ week15/my.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <wait.h>
+#include <signal.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/times.h>
+#include <time.h>
+#include <pthread.h>
+#include <limits.h>
+
+#define LOOP 10000000
+#define NUM 4 
+#define NT 100
